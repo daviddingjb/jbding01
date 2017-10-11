d4c4e47873d431442b4a27e68570c066bf666ecc
daviddingjb <157517301@qq.com>
2017-10-11 21:21:46 +0800

    jbding

--- com/study/demo001/selenium/demo01.py
+++ com/study/demo001/selenium/demo01.py
@@ -1,30 +1,60 @@
 #encoding=utf-8
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+class MyHTMLParser(HTMLParser):
+    container = ""
+    # def handle_starttag(self, tag, attrs):
+    #     print("Start tag:", tag)
+    #     for attr in attrs:
+    #         print("     attr:", attr)
+    #
+    # def handle_endtag(self, tag):
+    #     print("End tag  :", tag)
+
+    def handle_data(self, data):
+        print("Data     :", data)
+        self.container += data
+        return self.container
+
+    # def handle_comment(self, data):
+    #     print("Comment  :", data)
+    #
+    # def handle_entityref(self, name):
+    #     c = chr(name2codepoint[name])
+    #     print("Named ent:", c)
+    #
+    # def handle_charref(self, name):
+    #     if name.startswith('x'):
+    #         c = chr(int(name[1:], 16))
+    #     else:
+    #         c = chr(int(name))
+    #     print("Num ent  :", c)
+    #
+    # def handle_decl(self, data):
+    #     print("Decl     :", data)
 
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
-        print("1111")
         self.driver = webdriver.Chrome()
     def test_search_in_python_org(self):
-        print("2222")
         driver = self.driver
-        print("3333")
-        aaa = self.driver.get(url='http://beijing.cncn.com/jingdian/gugong/profile')
-        print("4444")
-        # ele = self.driver.find_element_by_xpath('//*[@id="wrapper"]/div[2]')
-        # print(ele.find_elements_by_id('wrapper'))
-        print(self.driver.page_source)
-        # print(driver.find_elements_by_id('wrapper'))
+        self.driver.get(url='http://beijing.cncn.com/jingdian/gugong/profile')
+
+        print("***************************************************************************************")
+        # if self.driver.page_source:
+        #     print(self.driver.page_source)
+        # else:
+        #     print("No results found.")
+        print("***************************************************************************************")
+
+        parser = MyHTMLParser()
+        parser.feed(self.driver.page_source)
+        print (parser.container)
+
         driver.close()
-        # self.assertIn("Python", driver.title)
-        # elem = driver.find_element_by_name("q")
-        # elem.send_keys("pycon")
-        # elem.send_keys(Keys.RETURN)
-        # assert "No results found." not in driver.page_source
-        # print(driver.page_source)
 
 if __name__=="__main__":
     unittest.main()

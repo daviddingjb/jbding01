41f8f39e4162f11c76dfcc5ae0ff4894bbcce706
daviddingjb <157517301@qq.com>
2017-10-12 09:41:12 +0800

    jbding

--- com/study/demo001/html/parser/parsing_websites_with_re_and_rullib.py
+++ com/study/demo001/html/parser/parsing_websites_with_re_and_rullib.py
@@ -1,27 +1,37 @@
 import urllib.request
 import urllib.parse
 import re
+from html.parser import HTMLParser
 
-def tryDecode(s, decoding="utf-8"):
-    try:
-        print(s.decode(decoding))
-    except UnicodeDecodeError as err:
-        print(err)
+class MyHTMLParser(HTMLParser):
+    container = ""
+    def handle_data(self, data):
+        # print("Data     :", data)
+        self.container += data
+        return self.container
 
 url = 'http://beijing.cncn.com/jingdian/gugong/profile'
-values = {'s':'basics','submit':'search'}
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-resp = urllib.request.urlopen(req)
-# resp = urllib.request.urlopen(url)
 
+############################################################
+#### method1
+# values = {'s':'basics','submit':'search'}
+# data = urllib.parse.urlencode(values)
+# data = data.encode('utf-8')
+# req = urllib.request.Request(url, data)
+# resp = urllib.request.urlopen(req)
+
+#### method2
+resp = urllib.request.urlopen(url)
+############################################################
 respData = resp.read().decode(resp.headers.get_content_charset())
 
 # print(respData)
-
-paragraphs = re.findall(r'<title>(.*?)</title>',str(respData))
+comment = re.compile(r'<div class="top">((?:.|\n)*?)<div class="sideR">')
+paragraphs = comment.findall(str(respData))
 for eachP in paragraphs:
-    print(eachP)
+    parser = MyHTMLParser()
+    parser.feed(eachP)
+    print (parser.container)
+    # print(eachP)
 
 
